f2a369493c0f22c72e3f008d1cdc0fe2c113234f
root <root@ip-172-31-21-123.ec2.internal>
2017-10-05 19:28:13 +0000

    publishing system info in webpage

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,16 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<html><h1>Hello World!! This is my second cookbook!! :)</h1></html>'
+  content "<html>
+		<h1>Hello World!! This is my second cookbook!! :)</h1>
+		<body>
+			HOSTNAME : #{node['ec2']['hostname']}<br>
+			PUBLIC IP : #{node['ec2']['public_ipv4']}<br>
+			IPADDRESS : #{node['ipaddress']}<br>
+			CPU : #{node['cpu']['0']['mhz']}<br>
+			MEMORY : #{node['memory']['total']}<br>
+		</body>
+	   </html>"
   action :create
 end
 
